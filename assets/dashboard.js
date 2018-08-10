2d3d0844ccc76e75274cd3cf21e2cea54675c619
edwinwalela <edwinwalela@gmail.com>
2018-08-10 21:32:13 +0300

    fix dashboard overflowing data

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
     // Fetch stage data
-
     $.ajax({
         method:'GET',
         url:'https://getma3.herokuapp.com/api/v1/stages/all',
@@ -8,16 +7,14 @@ $(document).ready(function(){
     }).done(function(result){
        $.map(result.response.data,function(stage){
          $('#data-feed').append(`
-         <div id="data-result">
+         <a target="_blank" href="https://www.google.com/maps/place/${stage.location[0]},${stage.location[1]}"><div class="data-result">
             <p><strong>Stage Name: </strong>${stage.name}</p>
             <p><strong>Description: </strong>${stage.desc}</p>
-            <p><strong>Nearby: </strong>${stage.nearby}</p>
+            <p><strong>Nearby: </strong>${stage.nearby}</p></a>
          `);
         })
        })
-
        // Fetch Team members
-
        $.ajax({
         method:'GET',
         url:'https://getma3.herokuapp.com/team/teammembers',
@@ -26,12 +23,10 @@ $(document).ready(function(){
        $.map(result,function(member){
            console.log(member);
          $('#team-feed').append(`
-         <div id="data-result">
+         <div class="data-result">
             <p style="display:inline-block"><strong></strong>${member.username}</p>
             <img style="display:inline-block; border-radius:50%" src="${member.thumb}"/>
          `);
         })
        })
     })
-
-
